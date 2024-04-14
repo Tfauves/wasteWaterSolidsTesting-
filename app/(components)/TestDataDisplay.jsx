d0d29a3bbe85444c9677770a44830ee2f322a73c
Travis J <tfauvelle@gmail.com>
2024-04-14 12:32:47 -0400

    added data display

---
+++ app/(components)/TestDataDisplay.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TestDataDisplay = ({ timeMarks }) => {
+  // Handle empty or null timeMarks
+  if (!timeMarks || Object.keys(timeMarks).length === 0) {
+    return <div>No time marks available</div>;
+  }
+
+  return (
+    <div>
+      <h2>Time Marks:</h2>
+      <ul>
+        {Object.entries(timeMarks).map(([key, value]) => (
+          <li key={key}>
+            <strong>{key}: </strong> {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TestDataDisplay;
