6b841df3c55c2930647b2aabc738a166f7a643c2
Travis J <tfauvelle@gmail.com>
2024-05-02 07:22:33 -0400

    bugged suspended test form

--- app/(models)/TotalSuspendedSolidsTest.js
+++ app/(models)/TotalSuspendedSolidsTest.js
@@ -7,74 +7,74 @@ mongoose.Promise = global.Promise;
 const SuspendedSolidsTestSchema = new Schema(
   {
     testNumber: String,
-    dishNumber: String,
-    testData: {
-      A_weightDryFilterAndSolids: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
-      B_weightOfCleanFilter: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
-      // A - B
-      C_weightOfDrySolids: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    // dishNumber: String,
+    // testData: {
+    //   A_weightDryFilterAndSolids: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
+    //   B_weightOfCleanFilter: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
+    //   // A - B
+    //   C_weightOfDrySolids: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      D_volumeOfSampleFiltered: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    //   D_volumeOfSampleFiltered: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      // C/D * 1,000,000
-      E_TSS: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    //   // C/D * 1,000,000
+    //   E_TSS: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      F_weightOfFilterAndAsh: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    //   F_weightOfFilterAndAsh: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      // F-B
-      G_weightOfAsh: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    //   // F-B
+    //   G_weightOfAsh: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      // C - G
-      H_weightOfVolatileSolids: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    //   // C - G
+    //   H_weightOfVolatileSolids: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      // H / D * 1,000,000
-      I_VSS: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
+    //   // H / D * 1,000,000
+    //   I_VSS: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
 
-      // I / E * 100
-      J_percentVolatileSolids: {
-        mixedLiquor: "",
-        influent: "",
-        final: "",
-      },
-    },
+    //   // I / E * 100
+    //   J_percentVolatileSolids: {
+    //     mixedLiquor: "",
+    //     influent: "",
+    //     final: "",
+    //   },
+    // },
 
-    notes: String,
+    // notes: String,
   },
   {
     timestamps: true,
@@ -83,6 +83,6 @@ const SuspendedSolidsTestSchema = new Schema(
 
 const TotalSuspendedSolidsTest =
   mongoose.models.TotalSuspendedSolidsTest ||
-  mongoose.model("Total Solids Test", SuspendedSolidsTestSchema);
+  mongoose.model("Total Suspended Solids Test", SuspendedSolidsTestSchema);
 
 export default TotalSuspendedSolidsTest;
--- app/api/TotalSolidsTest/route.js
+++ app/api/TotalSolidsTest/route.js
@@ -1,4 +1,4 @@
-import TotalSolidsTest from "@/app/(models)/TotalSolidsTest";
+import TotalSuspendedSolidsTest from "@/app/(models)/TotalSuspendedSolidsTest";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -6,7 +6,7 @@ export async function POST(req) {
     const body = await req.json();
     const tssReportData = body.tssFormData;
     console.log(body.tssFormData);
-    await TotalSolidsTest.create(tssReportData);
+    await TotalSuspendedSolidsTest.create(tssReportData);
 
     return NextResponse.json({ message: "Report Created" }, { status: 201 });
   } catch (error) {
@@ -16,7 +16,7 @@ export async function POST(req) {
 
 export async function GET() {
   try {
-    const tssReports = await TotalSolidsTest.find();
+    const tssReports = await TotalSuspendedSolidsTest.find();
     return NextResponse.json({ tssReports }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
