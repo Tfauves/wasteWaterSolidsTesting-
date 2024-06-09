456fff4c7a5754ce41213d7681958c07274b4b9c
Travis J <tfauvelle@gmail.com>
2024-06-09 13:54:53 -0400

    added NewReportPage and SolidsReportForm refactor

---
+++ app/NewReportPage/[id]/page.js
@@ -0,0 +1,38 @@
+import SolidsReportForm from "@/app/components/SolidsReportForm";
+import React from "react";
+
+const getReportById = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/SolidsReport/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to get Reports.");
+  }
+
+  return res.json();
+};
+
+const NewReportPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+
+  let updateReportData = {};
+
+  if (EDITMODE) {
+    updateReportData = await getReportById(params.id);
+    updateReportData = updateReportData.foundReport;
+    console.log(updateReportData);
+  } else {
+    updateReportData = {
+      _id: "new",
+    };
+  }
+
+  return (
+    <div>
+      <SolidsReportForm report={updateReportData} />
+    </div>
+  );
+};
+
+export default NewReportPage;
